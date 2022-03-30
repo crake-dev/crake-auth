bcc143958952bea75d54650e81abdd6df17acdb0
빵순이 <admin@heybread.app>
2022-03-30 14:34:51 +0900

    Initial commit

---
+++ src/main/java/app/crake/auth/CrakeAuthApplication.java
@@ -0,0 +1,13 @@
+package app.crake.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrakeAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrakeAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/crake/auth/CrakeAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package app.crake.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrakeAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
